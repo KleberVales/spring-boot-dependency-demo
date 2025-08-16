d76c4d23893d464652fd2bdbf02af63c5e6c5b78
Kleber Vales <klebervales.dev@gmail.com>
2025-08-16 10:55:49 -0300

    build: initialize project with java 21 and spring boot

---
+++ src/main/java/com/example/springbootdependencydemo/SpringBootDependencyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdependencydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDependencyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDependencyDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootdependencydemo/SpringBootDependencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdependencydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDependencyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
